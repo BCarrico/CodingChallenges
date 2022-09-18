700f5c4764a542f7da4588dfb4c94633e98550b5
BCarrico <BCarrico89@gmail.com>
2022-09-18 14:09:37 -0700

    Completed coding challenge for 09-18-2022

---
+++ CC 2022-09-18/cc.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+function century(year) {
+    return Math.ceil(year/100)
+  }
