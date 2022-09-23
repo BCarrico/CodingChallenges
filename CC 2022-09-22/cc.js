7e5f82b653d96fd0765a042ea79dec6035891c50
BCarrico <BCarrico89@gmail.com>
2022-09-23 12:26:01 -0700

    Completed coding challenge for 09-23-2022

--- CC 2022-09-22/cc.js
+++ CC 2022-09-22/cc.js
@@ -6,3 +6,25 @@
 function grow(x){
     return x.reduce((a, b) => a * b)
    }
+
+
+   function hero(bullets, dragons){
+    if (bullets === 0){
+        return false
+    } else if (bullets % dragons === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function hero(bullets, dragons){
+    return bullets >= dragons * 2
+  }
+
+//   each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+function last(x){
+    let strArray = x.split(" ")
+    return strArray.sort((a,b) => a.charAt(-1) > b.charAt(-1))
+}
