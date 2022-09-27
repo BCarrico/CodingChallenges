2b18456986d346df6d0ae66af516e262b96cb7cd
BCarrico <BCarrico89@gmail.com>
2022-09-27 12:30:32 -0700

    Completed coding challenge for 09-27-2022

---
+++ CC 2022-09-27/cc.js
@@ -0,0 +1,5 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+function isPalindrome(x) {
+    return x.toLowerCase() === x.split("").reverse().join("").toLowerCase()
+  }
