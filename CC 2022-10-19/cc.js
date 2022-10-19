00d6397accd9feb1c2139dc1033b450bbdff0c81
BCarrico <BCarrico89@gmail.com>
2022-10-19 09:55:18 -0700

    Completed coding challenge for 10-19-2022

---
+++ CC 2022-10-19/cc.js
@@ -0,0 +1,17 @@
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+// Your function will be tested with pre-made examples as well as random ones.
+
+// If you can, try writing it in one line of code.
+
+function findDifference(a, b) {
+    let asum = a.reduce((a,b) => a * b)
+     let bsum = b.reduce((a,b) => a * b)
+     if (bsum > asum){
+       return bsum - asum
+     } else {
+       return asum - bsum
+     }
+   }
