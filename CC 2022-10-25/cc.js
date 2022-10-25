04f1b328c77e69089910b928baa04ffd69c1b132
BCarrico <BCarrico89@gmail.com>
2022-10-25 10:16:05 -0700

    Completed coding challenge for 10-25-2022

---
+++ CC 2022-10-25/cc.js
@@ -0,0 +1,9 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+    let splitString = s.split(" ")
+    splitString.sort((a, b) => a.length - b.length)
+    return splitString[0].length
+  }
