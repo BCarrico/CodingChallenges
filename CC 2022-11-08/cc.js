411cbc7b31e184083cb28a6ece01e0e40f1aa76a
BCarrico <BCarrico89@gmail.com>
2022-11-08 08:53:10 -0800

    Completed coding challenge for 11-8-2022

---
+++ CC 2022-11-08/cc.js
@@ -0,0 +1,9 @@
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+function distinct(a) {
+    return resultArray = a.filter((num, index) => {
+      return a.indexOf(num) === index
+    })
+  }
