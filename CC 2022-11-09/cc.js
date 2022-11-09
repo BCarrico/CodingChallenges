01076601a9b9c9fec5a042b06145e96f0208a472
BCarrico <BCarrico89@gmail.com>
2022-11-09 09:38:15 -0800

    Completed coding challenge for 11-9-2022

---
+++ CC 2022-11-09/cc.js
@@ -0,0 +1,10 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+    let urlArray = url.split("#")
+    return urlArray[0]
+  }
