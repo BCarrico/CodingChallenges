35471d55597f6a77c99cfa7f468bc45c49f7e97c
bradley carrico <BCarrico89@gmail.com>
2022-11-28 09:45:21 -0800

    completed coding challenge for 11-28-2022

---
+++ CC 2022-11-28/cc.js
@@ -0,0 +1,18 @@
+// Task
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+function sortArray(array) {
+    let oddArray = array.filter(item => item % 2 != 0).sort((a,b)=>a-b)
+    for (let i = 0; i < array.length; i++){
+      if (array[i] % 2 == 0){
+        oddArray.splice(i, 0, array[i])
+      }
+    } 
+  
+    return(oddArray)
+  }
