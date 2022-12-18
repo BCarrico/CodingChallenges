0832fe371e64fd1595e6fcb9c39c11dcc11199a0
bradley carrico <BCarrico89@gmail.com>
2022-12-18 14:07:50 -0800

    Completed coding challenge for 12-18-2022

---
+++ CC 2022-12-18/cc.js
@@ -0,0 +1,15 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+    let array = str.split(" ")
+    let reverse = []
+    for (let i = 0; i < array.length; i++){
+      reverse.push(array[i].split("").reverse().join(""))
+    }
+     return reverse.join(" ")
+    
+   }
