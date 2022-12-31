234e98a77a2770b269df037ed269a822af81acc1
bradley carrico <BCarrico89@gmail.com>
2022-12-31 12:40:32 -0800

    Completed coding challenge for 12-31-2022

---
+++ CC 2022-12-31/cc.js
@@ -0,0 +1,14 @@
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
+
+// Create the combineNames function here
+function combineNames(first, last){
+    return `${first} ${last}`
+  }
