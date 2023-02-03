cb4b7be90d6a1a7630872178e84dc6628db11716
bradley carrico <BCarrico89@gmail.com>
2023-02-03 10:21:44 -0800

    Completed coding challenge for 02-03-2023

---
+++ CC 2023-02-03/cc.js
@@ -0,0 +1,17 @@
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
+
+// Assume that the input n will always be a positive integer.
+
+// Examples: (Input --> output)
+
+// 2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+// 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
+
+function sumCubes(n){
+    let result = 0
+   for (let i = 0; i <= n; i++){
+     result += Math.pow(n-i, 3)
+   }
+    return result
+  }
+
