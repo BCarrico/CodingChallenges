fbbb418badafc304a6a0cb635d708f1f6b5a90f7
bradley carrico <BCarrico89@gmail.com>
2023-02-14 14:56:53 -0800

    Completed coding challenge for 02-14-2023

--- CC 2023-02-14/cc.js
+++ CC 2023-02-14/cc.js
@@ -59,3 +59,38 @@ function createElement(content, tag='div', props={}) {
 function createUnorderedList(list) {
   return React.createElement('ul', {}, list.map((str, i) => React.createElement('li', {key : +i}, str)));
 }
+
+// United State of React
+// Some say The United States has never been so divided. This Kata is a chance for you to unite a once great nation and MAKE AMERICA CODE AGAIN!
+
+// What's Involved
+// State
+// Changing State
+// Conditional Rendering based on state
+// The challenge
+// You will be given a component called States. You will need to establish an initial state for the component called united and for it to be set to false.
+
+// Then write a unite function that changes this state to true.
+
+// Then conditionally render a bespoke message in the component.
+
+// If the States are united then display "Code for everyone"
+
+// If the States aren't united then display "Make America code again"
+
+import React from 'react';
+
+export class States extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      united: false
+    }
+  }
+  unite (){
+    this.setState({united: !this.state.check})
+  }
+  render() {
+    return <div className="status">{this.state.united ? "Code for everyone" : "Make America code again"}</div>
+  } 
+}
