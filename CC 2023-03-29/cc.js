14e7c24a8f9a65320ac261cd23e99decaa89a390
Brad Carrico <bcarrico89@gmail.com>
2023-03-29 05:57:23 -0700

    Completed coding challenge 03-29-2023

---
+++ CC 2023-03-29/cc.js
@@ -0,0 +1,12 @@
+// Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+// (the dedicated builtin(s) functionalities are deactivated)
+
+reverse = function(array) {
+ 
+    let result = []
+    for (let i = array.length-1; i >= 0; i--){
+      result.push(array[i])
+    }
+    return result
+  }
