8b676b1fbe9a2c5b22ac24b3932ea5c9a8e0f161
bradley carrico <BCarrico89@gmail.com>
2023-05-05 07:22:09 -0700

    Completed coding challenge for 05-05-2023

---
+++ CC 2023-05-05/cc.js
@@ -0,0 +1,10 @@
+// Write a function that reverses the bits in an integer.
+
+// For example, the number 417 is 110100001 in binary. Reversing the binary is 100001011 which is 267.
+
+// You can assume that the number is not negative.
+
+function reverseBits (n) {
+    let num = n.toString(2).split("").reverse().join("")
+    return parseInt(num, 2)
+  }
