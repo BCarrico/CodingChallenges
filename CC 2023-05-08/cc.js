36a40ce95cc0f40361eed9ecc7e43e97f64fa22c
bradley carrico <BCarrico89@gmail.com>
2023-05-08 08:59:00 -0700

    Completed coding challenge for 05-08-2023

---
+++ CC 2023-05-08/cc.js
@@ -0,0 +1,13 @@
+// You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+
+// It is guaranteed that a and b are both present in arr.
+
+function consecutive(arr, a, b) {
+    let num1 = arr.indexOf(a)
+    let num2 = arr.indexOf(b)
+    if (num1 === num2 +1 || num1 === num2 - 1) {
+      return true
+    } else {
+      return false
+    }
+  }
