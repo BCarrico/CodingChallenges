84d6f887ebd37637eb36ae1ebf7490045a5003e5
bradley carrico <BCarrico89@gmail.com>
2023-05-17 10:44:07 -0700

    Completed coding challenge for 05-17-2023

---
+++ CC 2023-05-17/cc.cs
@@ -0,0 +1,24 @@
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+using System;
+
+public class Kata
+{
+  public static int Litres(double time)
+  {
+    
+    return (int)Math.Floor(time * 0.5);
+  }
+}
