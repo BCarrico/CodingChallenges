c01d1eea926fca328dcbca8b954e1ad8a5f9ced3
bradley carrico <BCarrico89@gmail.com>
2023-05-21 21:38:24 -0700

    Completed coding challenge for 05-21-2023

---
+++ CC 2023-05-21/cc.js
@@ -0,0 +1,7 @@
+// In JavaScript, your code is running in a function, let us step by step complete your first JS function.
+
+function helloWorld(){
+    let str = "Hello World!"
+    console.log(str)
+    return str
+  }//refer to the example and write your first JS function
