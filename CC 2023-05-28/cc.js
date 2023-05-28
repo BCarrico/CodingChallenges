2f34fe20429d654c348623e0f8dfb8106fcc7d7c
bradley carrico <BCarrico89@gmail.com>
2023-05-28 12:05:02 -0700

    Completed coding challenge for 05-28-2023

---
+++ CC 2023-05-28/cc.js
@@ -0,0 +1,14 @@
+// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+// Note: the input will not be empty.
+
+// Examples
+// "1plus2plus3plus4"  --> "10"
+// "1plus2plus3minus4" -->  "2"
+
+function calculate(str) {
+    str = str.replace(/plus/g, '+').replace(/minus/g, '-');
+      
+      
+      return eval(str).toString();
+    }
